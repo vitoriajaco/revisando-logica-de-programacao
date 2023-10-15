b5ae30ce7613cb27e03e8619bcd2add198e02254
vitoriajaco <vitoriajaco@gmail.com>
2023-10-14 21:32:09 -0300

    Versao 2 sem scanner

--- src/main/java/com/example/demo/exercicioComUsoDeFor/ExercicioComFor.java
+++ src/main/java/com/example/demo/exercicioComUsoDeFor/ExercicioComFor.java
@@ -3,29 +3,30 @@ package com.example.demo.exercicioComUsoDeFor;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+
 
 public class ExercicioComFor {
-
-  /*  private static final Scanner scanner = new Scanner(System.in);*/
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Digite um numero ");
         int N = Integer.parseInt(bufferedReader.readLine().trim());
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
         int i = 0;
-        /*int  N = scanner.nextInt();*/
 
 
+        if (N >= 2 && N <= 10) {
+            for (i = 1; i <= 10; i++) {
+                int result = (N * i);
+                System.out.println(N + "x" + i + "=" + result);
 
-        for(i= 1; i <= 10; i++){
-            int result = ( N * i);
-            System.out.println(N + "*"+ i + "=" + result);
-
-
+            }
             bufferedReader.close();
+
+        } else {
+            System.out.println("Numero fora do intervalo de 2 a 10");
         }
+
     }
 
 }
+
