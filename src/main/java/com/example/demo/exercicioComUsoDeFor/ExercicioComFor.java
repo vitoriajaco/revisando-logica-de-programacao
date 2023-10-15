70db3a6583d2bc0986cd7c3343393d0f5439b0c7
vitoriajaco <vitoriajaco@gmail.com>
2023-10-14 21:43:51 -0300

    Exercicio HackerHank com for finalizado

--- src/main/java/com/example/demo/exercicioComUsoDeFor/ExercicioComFor.java
+++ src/main/java/com/example/demo/exercicioComUsoDeFor/ExercicioComFor.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*Task
+        Given an integer, N , print its first 10 multiples. Each multiple N (where 1 <= i <= 10) should be printed on a new line in the form: N x i = result.
+Constraint: 2 <= N <= 20*/
+
 
 public class ExercicioComFor {
 
@@ -14,16 +18,16 @@ public class ExercicioComFor {
         int i = 0;
 
 
-        if (N >= 2 && N <= 10) {
+        if (N >= 2 && N <= 20) {
             for (i = 1; i <= 10; i++) {
                 int result = (N * i);
-                System.out.println(N + "x" + i + "=" + result);
+                System.out.println(N + " x " + i + " = " + result);
 
             }
             bufferedReader.close();
 
         } else {
-            System.out.println("Numero fora do intervalo de 2 a 10");
+            System.out.println("Numero fora do intervalo de 2 a 20");
         }
 
     }
