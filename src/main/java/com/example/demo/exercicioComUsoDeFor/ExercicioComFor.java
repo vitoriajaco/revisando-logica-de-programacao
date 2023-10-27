a8f96d44ac9802f194eac4b92e9f1666c72f7752
vitoriajaco <vitoriajaco@gmail.com>
2023-10-27 15:55:07 -0300

    Melhorando o codigo

--- src/main/java/com/example/demo/exercicioComUsoDeFor/ExercicioComFor.java
+++ src/main/java/com/example/demo/exercicioComUsoDeFor/ExercicioComFor.java
@@ -1,5 +1,7 @@
 package com.example.demo.exercicioComUsoDeFor;
 
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,25 +14,32 @@ Constraint: 2 <= N <= 20*/
 public class ExercicioComFor {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Digite um numero ");
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
-        int i = 0;
 
-
-        if (N >= 2 && N <= 20) {
-            for (i = 1; i <= 10; i++) {
-                int result = (N * i);
-                System.out.println(N + " x " + i + " = " + result);
-
-            }
-            bufferedReader.close();
-
-        } else {
-            System.out.println("Numero fora do intervalo de 2 a 20");
-        }
+        validaSeNAtentendeCondicao();
 
     }
 
-}
+        public static void validaSeNAtentendeCondicao() throws IOException {
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Digite um numero ");
+            int N = Integer.parseInt(bufferedReader.readLine().trim());
+            int i = 0;
+
+
+            if (N >= 2 && N <= 20) {
+                for (i = 1; i <= 10; i++) {
+                    int result = (N * i);
+                    System.out.println(N + " x " + i + " = " + result);
+
+                }
+                bufferedReader.close();
+
+            } else {
+                System.out.println("Numero fora do intervalo de 2 a 20");
+            }
+
+        }
+    }
+
 
