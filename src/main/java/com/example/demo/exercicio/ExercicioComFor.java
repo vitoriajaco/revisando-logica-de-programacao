5750e14c932a5ade7a49c6dd9203b1f0d32775e4
vitoriajaco <vitoriajaco@gmail.com>
2023-10-31 14:49:47 -0300

    Classe de testes

--- src/main/java/com/example/demo/exercicio/ExercicioComFor.java
+++ src/main/java/com/example/demo/exercicio/ExercicioComFor.java
@@ -1,4 +1,4 @@
-package com.example.demo.exercicioComUsoDeFor;
+package com.example.demo.exercicio;
 
 
 
---
+++ src/test/java/com/example/demo/exercicio/ExercicioComForTest.java
@@ -0,0 +1,38 @@
+package com.example.demo.exercicio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@SpringBootTest
+class ExercicioComForTest {
+    @Mock
+    BufferedReader bufferedReader;
+
+    @Test
+    public void validaSeNAtendeACondicaoTest() throws IOException {
+        Mockito.when(bufferedReader.readLine()).thenReturn("2");
+        boolean result =  ExercicioComFor.validaSeNAtentendeCondicao(bufferedReader);
+        Assertions.assertEquals(false, result);
+
+    }
+
+    @Test
+    public void validaSeNNaoAtendeACondicaoTest() throws IOException {
+        Mockito.when(bufferedReader.readLine()).thenReturn("1");
+        boolean result =  ExercicioComFor.validaSeNAtentendeCondicao(bufferedReader);
+        Assertions.assertEquals(true, result);
+
+    }
+
+
+}
+
+
+
+
